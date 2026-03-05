40fbfd90f863552d78808d28fa4d4549de39f0a0
황예찬 <skals7269@gmail.com>
2026-03-05 14:04:27 +0900

    FEAT : add user entity and repository

---
+++ be/spring/src/main/java/com/woojudraw/domain/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.woojudraw.domain.user.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 50)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, length = 50)
+	private String nickname;
+
+	private LocalDateTime lastLoginAt;
+
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+
+	@PrePersist
+	public void prePersist() {
+		this.createdAt = LocalDateTime.now();
+	}
+
+	public void updateLastLogin() {
+		this.lastLoginAt = LocalDateTime.now();
+	}
+}
