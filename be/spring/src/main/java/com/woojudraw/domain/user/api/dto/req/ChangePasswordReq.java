0a16198f18acb0d4fafed91bd540ef6307b26139
황예찬 <skals7269@gmail.com>
2026-03-18 09:47:36 +0900

    fix : ChangePasswordReq min size

--- be/spring/src/main/java/com/woojudraw/domain/user/api/dto/req/ChangePasswordReq.java
+++ be/spring/src/main/java/com/woojudraw/domain/user/api/dto/req/ChangePasswordReq.java
@@ -12,6 +12,6 @@ public class ChangePasswordReq {
 	private String currentPassword;
 
 	@NotBlank(message = "새 비밀번호는 필수입니다.")
-	@Size(min = 0, max = 20, message = "비밀번호는 8~20자 사이여야 합니다.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이여야 합니다.")
 	private String newPassword;
 }
