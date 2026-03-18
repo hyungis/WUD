d700047cdc5c2708720dcad4a00aa67c2873a86f
HyunGi Hong <ghd3466@naver.com>
2026-03-18 12:50:54 +0900

    FEAT : 심층 검사 주간 가용성 처리 로직 구현

--- be/spring/src/main/java/com/woojudraw/domain/deep/repository/DeepSessionRepository.java
+++ be/spring/src/main/java/com/woojudraw/domain/deep/repository/DeepSessionRepository.java
@@ -1,16 +1,27 @@
 package com.woojudraw.domain.deep.repository;
 
+import java.time.OffsetDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.woojudraw.domain.deep.entity.DeepSession;
+import com.woojudraw.domain.deep.entity.DeepStatus;
 import com.woojudraw.domain.deep.entity.DeepSubmission;
+import com.woojudraw.domain.deep.entity.DeepType;
 
 public interface DeepSessionRepository extends JpaRepository<DeepSession, Long> {
 
 	Optional<DeepSession> findByIdAndUserId(Long id, Long userId);
 
 	List<DeepSession> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+
+	boolean existsByUserIdAndDeepTypeAndStatusInAndCreatedAtAfter(
+		Long userId,
+		DeepType deepType,
+		Collection<DeepStatus> statuses,
+		OffsetDateTime after
+	);
 }
